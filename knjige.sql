90e036abd4e1c94fd8d4d1ef06570e139c922d6f
Jovana Aleksic <jaleksic23@gmail.com>
2023-01-19 12:15:41 +0100

    Nova baza

--- knjige.sql
+++ knjige.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 13, 2021 at 02:14 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Host: localhost:3307:3307
+-- Generation Time: Jan 13, 2023 at 10:44 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,27 +26,56 @@ USE `knjige`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `clanovi`
+--
+
+DROP TABLE IF EXISTS `clanovi`;
+CREATE TABLE `clanovi` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `prezime` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefon` varchar(50) NOT NULL,
+  `adresa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clanovi`
+--
+
+INSERT INTO `clanovi` (`id`, `ime`, `prezime`, `email`, `telefon`, `adresa`) VALUES
+(1, 'Jovana', 'Aleksic', 'jaleksic@gmail.com', '0653259874', 'Jove Ilica 52'),
+(4, 'Stefan', 'Markovic', 'sm@gmail.com', '0633654792', 'Zlatiborska 46'),
+(5, 'Ana', 'Ivanovic', 'ai@gmail.com', '0666549823', 'Makedonska 33'),
+(8, 'Petar', 'Petrovic', 'pp@gmail.com', '0613459761', 'Paunova 64');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `prijave`
 --
 
 DROP TABLE IF EXISTS `prijave`;
 CREATE TABLE `prijave` (
-  `id` int(11) NOT NULL,
+  `prijava_id` int(11) NOT NULL,
   `naziv` varchar(255) NOT NULL,
   `autor` varchar(255) NOT NULL,
   `drzava` varchar(255) NOT NULL,
   `zanr` varchar(255) NOT NULL,
-  `datum` date NOT NULL DEFAULT current_timestamp()
+  `datum` date NOT NULL DEFAULT current_timestamp(),
+  `idClana` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `prijave`
 --
 
-INSERT INTO `prijave` (`id`, `naziv`, `autor`, `drzava`, `zanr`, `datum`) VALUES
-(1, 'The Hobbit', 'J. R. R. Tolkien', 'USA', 'Fantasy', '2022-11-15'),
-(5, 'Robinson Crusoe', 'Daniel Defo', 'FRA', 'Adventure', '2022-10-10'),
-(6, 'Kad su cvetale tikve', 'Dragoslav Mihailović', 'SRB', 'Drama', '2022-10-1');
+INSERT INTO `prijave` (`prijava_id`, `naziv`, `autor`, `drzava`, `zanr`, `datum`, `idClana`) VALUES
+(63, 'Harry Potter and the Goblet of Fire', 'J.K.Rowling', 'UK', 'Fantastika', '2023-01-04', 5),
+(64, 'Silmarillion 2', 'J. R. R. Tolkien', 'SAD', 'Fantastika', '2023-01-04', 4),
+(67, 'Šesta faza sna', 'Bernar Verber', 'Francuska', 'Avanturistički', '2022-12-23', 8),
+(68, 'The Baltimore Boys', 'Joël Dicker', 'Switzerland', 'Drama', '2022-12-01', 5),
+(69, 'Sreća je piti čaj s tobom', 'Mamen Sančes', 'Španija', 'Ljubavni', '2022-12-27', 1);
 
 -- --------------------------------------------------------
 
@@ -56,7 +85,7 @@ INSERT INTO `prijave` (`id`, `naziv`, `autor`, `drzava`, `zanr`, `datum`) VALUES
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -65,7 +94,7 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
+INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
 (1, 'admin', 'admin');
 
 --
@@ -73,32 +102,55 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `clanovi`
+--
+ALTER TABLE `clanovi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `prijave`
 --
 ALTER TABLE `prijave`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`prijava_id`),
+  ADD KEY `prijave_ibfk_1` (`idClana`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `clanovi`
+--
+ALTER TABLE `clanovi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `prijave`
 --
 ALTER TABLE `prijave`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `prijava_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `prijave`
+--
+ALTER TABLE `prijave`
+  ADD CONSTRAINT `prijave_ibfk_1` FOREIGN KEY (`idClana`) REFERENCES `clanovi` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
